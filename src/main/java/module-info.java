365153822dfbb992b7525184a6ce449f7f119485
Julian Ramirez <jramirezramirez2005@gmail.com>
2026-03-09 08:06:29 -0500

    Resolve merge conflict: keep main version (com.example)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,8 @@
-module com.guessrule {
+module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
-    requires java.sql;
 
-    opens com.guessrule to javafx.fxml;
-    opens com.guessrule.controller to javafx.fxml;
-    opens com.guessrule.model to javafx.fxml;
-
-    exports com.guessrule;
-    exports com.guessrule.controller;
-    exports com.guessrule.model;
+    opens com.example to javafx.fxml;
+    exports com.example;
 }
