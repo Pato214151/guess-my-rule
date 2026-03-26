82570da119719ba99f65850bd8bba0266423a9a5
Baabsy <vnustes94@gmail.com>
2026-03-26 00:36:17 -0500

    Ingegración de clases a develop

--- src/main/java/com/example/util/NavigationException.java
+++ src/main/java/com/example/util/NavigationException.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:src/main/java/com/example/exception/NavigationException.java
-package com.example.exception;
-========
 package com.example.util;
->>>>>>>> origin/main:src/main/java/com/example/util/NavigationException.java
 
 public class NavigationException extends RuntimeException {
     public NavigationException(String message, Throwable cause) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,14 +2,14 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
-    opens com.example to javafx.fxml;
-    opens com.example.controller to javafx.fxml;
-    opens com.example.model to javafx.fxml;
+    opens com.example             to javafx.fxml, javafx.graphics;
+    opens com.example.Controller  to javafx.fxml, javafx.base;
+    opens com.example.Model       to javafx.fxml;
+    opens com.example.util        to javafx.fxml;
 
     exports com.example;
-    exports com.example.controller;
-    exports com.example.model;
-    exports com.example.exception;
-    exports com.example.util;
+    exports com.example.Controller;
+    exports com.example.Model;
 }
